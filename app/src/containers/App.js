6686b5b73eab28ec69698e5795d7e95ea7ac3ed8
Marina <pidoimamarina@gmail.com>
2018-10-29 23:01:42 +0200

    add react select

--- app/src/containers/App.js
+++ app/src/containers/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Grid from '@material-ui/core/Grid';
+import Select from 'react-select';
 import '../App.css';
 import WeatherCard from '../components/WeatherCard/WeatherCard';
 
@@ -20,16 +21,28 @@ function getDayOfWeek(day){
     };
 }
 
+const options = () => cities.map(city => ({ value: city, label: city }));
+
 class App extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            selectedOptions: options(),
+        };
+    }
+
     componentDidMount() {
         this.props.fetchCities();
     }
 
     createChunks() {
+        const selectedCities = this.state.selectedOptions.map(opt => opt.label);
+        const filteredArrayOfCities = this.props.cities.filter(city => selectedCities.includes(city.cityName));
         let i,j,tempArray,chunk = 4;
         let res = [];
-        for (i = 0,j = this.props.cities.length; i < j; i += chunk) {
-            tempArray = this.props.cities.slice(i,i + chunk);
+        for (i = 0,j = filteredArrayOfCities.length; i < j; i += chunk) {
+            tempArray = filteredArrayOfCities.slice(i,i + chunk);
             res.push(
                 this.renderCities(tempArray, cities.slice(i, i + chunk))
             );
@@ -37,11 +50,11 @@ class App extends Component {
         return res;
     }
 
-    renderCities(chunk, cityNames) {
+    renderCities(chunk) {
         const date = new Date();
         const currentDay = date.getDay();
         const hour = date.getHours();
-        return chunk.map((city, index) => {
+        return chunk.map(city => {
             const dayOfWeek = getDayOfWeek(currentDay);
             const periods = city.response[0].periods;
             return (
@@ -50,7 +63,7 @@ class App extends Component {
                         temperature={periods[0].avgTempC}
                         weather={periods[0].weather || periods[0].weatherPrimary}
                         timeOfDay={hour < 20 && hour > 6 ? 'day' : 'night' }
-                        city={cityNames[index]}
+                        city={city.cityName}
                         date={date.getDate()}
                         month={monthMap[date.getMonth()]}
                         day={dayMap[currentDay].toUpperCase()}
@@ -64,18 +77,33 @@ class App extends Component {
                 </Grid>);
         });
     }
-  render() {
-    return (
-        <Grid
-            container
-            direction="row"
-            spacing={32}
-            alignContent="space-around"
-        >
-            {this.createChunks.bind(this)()}
-        </Grid>
-    );
-  }
+
+    handleChange = selectedOptions => {
+        this.setState({selectedOptions});
+    };
+
+    render() {
+        return (
+            <React.Fragment>
+                <Select
+                    value={this.state.selectedOptions}
+                    onChange={this.handleChange}
+                    options={options()}
+                    isMulti
+                    isSearchable
+                    placeholder="Start typing city name"
+                />
+                <Grid
+                    container
+                    direction="row"
+                    spacing={32}
+                    alignContent="space-around"
+                >
+                    {this.createChunks.bind(this)()}
+                </Grid>
+            </React.Fragment>
+        );
+    }
 }
 
 const mapStateToProps = state => ({
--- app/src/reducer.js
+++ app/src/reducer.js
@@ -1,13 +1,18 @@
 import * as actionsTypes from './actionTypes';
 import { combineReducers } from 'redux';
+import { cities } from './const';
 
 const initialState = {
     cities: [],
 };
 
+const addCityNames = arr => {
+    return arr.map((city, index) => ({...city, cityName: cities[index]}));
+};
+
 const reducer = (state = initialState, actions) => {
     switch(actions.type) {
-        case actionsTypes.ADD_CITIES: { return Object.assign({}, state, { cities: actions.payload.response.responses }) }
+        case actionsTypes.ADD_CITIES: { return Object.assign({}, state, { cities: addCityNames(actions.payload.response.responses) }) }
         default: return state;
     }
     return state;
